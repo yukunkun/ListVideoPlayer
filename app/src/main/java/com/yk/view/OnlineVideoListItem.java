15510b0a8fa20e6b4130ab14475232a9ef257c9a
yukun <www.1163459047@qq.com>
2017-10-12 15:02:14 +0800

    first commit

---
+++ app/src/main/java/com/yk/view/OnlineVideoListItem.java
@@ -0,0 +1,27 @@
+package com.yk.view;
+import com.volokh.danylo.video_player_manager.manager.VideoPlayerManager;
+import com.volokh.danylo.video_player_manager.meta.MetaData;
+import com.volokh.danylo.video_player_manager.ui.VideoPlayerView;
+/**
+ * Created by yukun on 17-10-12.
+ */
+
+public class OnlineVideoListItem extends VideoListItem {
+    private final String mOnlineUrl; // 资源文件描述
+
+    public OnlineVideoListItem(
+            VideoPlayerManager<MetaData> videoPlayerManager,
+            String title,
+            String imageResource,
+            String onlineUrl
+    ) {
+        super(videoPlayerManager, title, imageResource);
+
+        mOnlineUrl = onlineUrl;
+    }
+
+    @Override
+    public void playNewVideo(MetaData currentItemMetaData, VideoPlayerView player, VideoPlayerManager<MetaData> videoPlayerManager) {
+        videoPlayerManager.playNewVideo(currentItemMetaData, player, mOnlineUrl);
+    }
+}
